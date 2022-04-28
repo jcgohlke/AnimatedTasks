2a15f03d06731e7f62905c79c7cc67f4141ba918
Ben Gohlke <jcgohlke@me.com>
2022-04-28 16:35:30 -0400

    Initial Commit

---
+++ AnimatedTasks/AnimatedTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTasksApp.swift
+//  AnimatedTasks
+//
+//  Created by Joben Gohlke on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
